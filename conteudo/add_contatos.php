59a5c3f536cf3864f98f35d4794f511a69a44f7e
Weslley Feitosa Barbosa <weslleybarbosa662@gmail.com>
2025-06-02 20:30:01 -0300

    Inital commit

--- conteudo/add_contatos.php
+++ conteudo/add_contatos.php
@@ -2,17 +2,17 @@
     $name_contact=$_POST["name_contact"];
     $number_contact=$_POST["number_contact"];
 
-    $conn=new mysqli("localhost","root","","lista de contatos");
+    $conn=new mysqli("localhost","root","","lista_contatos");
     if($conn->connect_error){
         die($conn->connect_error);
     }
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $sql="INSERT INTO contatos(nome,numero) VALUES(?,?)";
+        $sql="INSERT INTO contato(nome,numero) VALUES(?,?)";
         $stmt=$conn->prepare($sql);
         $stmt->bind_param("ss",$name_contact,$number_contact);
         if($stmt->execute()){
-            header("location: https://www.youtube.com");
+            header("location: contato_salvo.php");
         }
         
     }
