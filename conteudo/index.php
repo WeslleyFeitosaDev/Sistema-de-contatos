24c65077c96182e30e6330cd358bbd5005b5ce48
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-02 14:39:47 -0300

    Update index.php

--- conteudo/index.php
+++ conteudo/index.php
@@ -7,6 +7,16 @@
     <title>Adicionar contatos</title>
 </head>
 <body>
-    
+    <div class="caixa_adicionar_contatos">
+        <div class="titulo">
+            <h1>Adicionar Contatos</h1>
+        </div>
+
+        <form action="" method="post">
+            <input type="text" name="name_contact" id="name_contact" placeholder="Digite o nome do contato">
+            <input type="text" name="number_contact" id="number_contact" placeholder="Digite o número">
+            <input type="submit" value="Adicionar">
+        </form>
+    </div>
 </body>
 </html>
