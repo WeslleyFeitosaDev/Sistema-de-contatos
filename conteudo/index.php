7a960a7c1b4503d0ec3fdc702f44cdc854a0e9af
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-03 10:34:20 -0300

    Inital commit

--- conteudo/index.php
+++ conteudo/index.php
@@ -8,14 +8,18 @@
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
+    <?php
+        require_once "add_contatos.php";
+    ?>
     <div class="caixa_adicionar_contatos">
         <div class="titulo">
             <h1>Adicionar Contatos</h1>
         </div>
-
-        <form action="add_contatos.php" method="post">
+        
+        <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
             <input type="text" name="name_contact" id="name_contact" placeholder="Digite o nome do contato" required>
             <input type="text" name="number_contact" id="number_contact" placeholder="Digite o número" required>
+            <p><?=$mensagem?></p>
             <input type="submit" value="Adicionar">
         </form>
     </div>
