7b7faad49e2ded4cd03f23c4eb5cb92e47ca757d
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-03 10:35:43 -0300

    Inital commit

--- conteudo/add_contatos.php
+++ conteudo/add_contatos.php
@@ -6,14 +6,24 @@
     if($conn->connect_error){
         die($conn->connect_error);
     }
-
+    $mensagem="";
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $sql="INSERT INTO contato(nome,numero) VALUES(?,?)";
-        $stmt=$conn->prepare($sql);
-        $stmt->bind_param("ss",$name_contact,$number_contact);
-        if($stmt->execute()){
-            header("location: contato_salvo.php");
+        $sql_verificao="SELECT id FROM contato WHERE numero=?";
+        $stmt_verificao=$conn->prepare($sql_verificao);
+        $stmt_verificao->bind_param("s",$number_contact);
+        $stmt_verificao->execute();
+        $stmt_verificao->store_result();
+        if($stmt_verificao->num_rows>0){
+            $mensagem="Esse número já está salvo";
+        }else{
+            $sql="INSERT INTO contato(nome,numero) VALUES(?,?)";
+            $stmt=$conn->prepare($sql);
+            $stmt->bind_param("ss",$name_contact,$number_contact);
+            if($stmt->execute()){
+                header("location: contato_salvo.php");
+            }
         }
-        
     }
+        
+        
 ?>
